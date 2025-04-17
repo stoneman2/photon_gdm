4ebd8eaeb6e840a21ef1dec6ce822d95c1108981
Stoneman <sirckelbrad@gmail.com>
2025-04-17 19:12:15 +0700

    Player Controller

--- Assets/Script/PlayerSpawnerController.cs
+++ Assets/Script/PlayerSpawnerController.cs
@@ -5,17 +5,17 @@ public class PlayerSpawnerController : NetworkBehaviour, IPlayerJoined
 {
     [SerializeField] private NetworkPrefabRef PlayerNetworkPrefab = NetworkPrefabRef.Empty;
     [SerializeField] private Transform[] spawnPoint;
-    public override void Spawned()
-    {
-        // base.Spawned();
-        if (Runner.IsServer)
-        {
-            foreach (var item in Runner.ActivePlayers)
-            {
-                SpawnPlayer(item);
-            }
-        }
-    }
+    // public override void Spawned()
+    // {
+    //     // base.Spawned();
+    //     if (Runner.IsServer)
+    //     {
+    //         foreach (var item in Runner.ActivePlayers)
+    //         {
+    //             SpawnPlayer(item);
+    //         }
+    //     }
+    // }
     public void PlayerJoined(PlayerRef player)
     {
         SpawnPlayer(player);
@@ -26,8 +26,10 @@ public class PlayerSpawnerController : NetworkBehaviour, IPlayerJoined
         if (Runner.IsServer)
         {
             int index = player.AsIndex % spawnPoint.Length;
-            var spawnPoint = spawnPoint[player].transform.position;
-            Runner.Spawn(PlayerNetworkPrefab, Vector3.zero, Quaternion.identity.player);
+            var newSpawnPoint = this.spawnPoint[index].transform.position;
+            var PlayerObject = Runner.Spawn(PlayerNetworkPrefab, newSpawnPoint, Quaternion.identity, player);
+
+            Runner.SetPlayerObject(player, PlayerObject);
         }
     }
 }
